6ee7362d7e6333569d302f7c0df7cd2795a5eaf3
Laurent Bossavit <laurent.git@bossavit.com>
2019-12-06 08:42:25 +0100

    Refactor writes

--- IntCode.hs
+++ IntCode.hs
@@ -24,14 +24,14 @@ run inputs program = outputs $ untilStable $ iterate step (0, program, inputs, [
 
 step :: State -> State
 step state@(pos, program, inputs, outputs) = case op of
-        1 -> (next 4, replace output (x+y) program, inputs, outputs)
-        2 -> (next 4, replace output (x*y) program, inputs, outputs)
-        3 -> (next 2, replace (addr 1) (head inputs) program, inputs, outputs)
+        1 -> (next 4, write result (x+y), inputs, outputs)
+        2 -> (next 4, write result (x*y), inputs, outputs)
+        3 -> (next 2, write (addr 1) (head inputs), inputs, outputs)
         4 -> (next 2, program, inputs, outputs ++ [fetch 1])
         5 -> (if x /= 0 then fromInteger y else next 3, program, inputs, outputs)
         6 -> (if x == 0 then fromInteger y else next 3, program, inputs, outputs)
-        7 -> (next 4, replace output (if x < y then 1 else 0) program, inputs, outputs)
-        8 -> (next 4, replace output (if x == y then 1 else 0) program, inputs, outputs)
+        7 -> (next 4, write result (if x < y then 1 else 0), inputs, outputs)
+        8 -> (next 4, write result (if x == y then 1 else 0), inputs, outputs)
         99 -> state
     where next count = pos + count
           opcode = (program !! pos)
@@ -41,7 +41,8 @@ step state@(pos, program, inputs, outputs) = case op of
           immediate n = program !! (pos+n)
           addr n = fromInteger $ immediate n
           value n = program !! (addr n)
+          write address value = replace address value program
           -- shortcuts for opcodes 1, 2
-          output = addr 3
+          result = addr 3
           x = fetch 1
           y = fetch 2
