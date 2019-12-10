4cb0a05d03e3244856bc1624141ce08ab7c1a8d5
Laurent Bossavit <laurent.git@bossavit.com>
2019-12-10 16:17:09 +0100

    Animate it

---
+++ Day10Animated.hs
@@ -0,0 +1,38 @@
+module Day10PartTwo where
+
+import Common
+import Data.List
+import Data.Char
+import Data.Foldable
+import Day10
+import Day10PartOne hiding (main)
+
+import System.Console.ANSI
+import Control.Concurrent
+
+vaporizeOrder :: (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+vaporizeOrder station field = concat $ transpose $ map (sortOn (distance station)) $ sightLines field station
+
+display :: [String] -> [(Int,Int)] -> [String]
+display lines targets = foldl insert lines (indexed targets)
+  where insert lines (index, (x,y)) = replace y (replace x (intToDigit (index+1)) (lines !! y)) lines
+
+displayOne :: [String] -> (Int,Int) -> [String]
+displayOne lines (x,y) = replace y ((replace x '.') (lines !! y)) lines
+  
+main = do
+    contents <- readFile "Day10Sample5.txt"
+    let field = asteroidCoordinates contents
+    let frames = vaporizeOrder (bestOf field) field
+    foldlM frame (lines contents) frames
+    return ()
+
+frame :: [String] -> (Int, Int) -> IO [String]
+frame strings target = do
+    setCursorPosition 0 0
+    clearScreen
+    threadDelay 10000
+    let nextFrame = displayOne strings target
+    putStrLn $ unlines $ nextFrame
+    threadDelay 30000
+    return nextFrame
