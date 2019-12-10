8fdaafa9ce2ea2caa0aa8d1d60974c1eb9ecc597
Laurent Bossavit <laurent.git@bossavit.com>
2019-12-10 13:57:00 +0100

    Simplify with atan2

--- Day10PartTwo.hs
+++ Day10PartTwo.hs
@@ -16,7 +16,7 @@ vaporizeOrder station = concat . transpose . map (sortOn (distance station)) . g
 clockwiseFrom center field = sortOn (angle center) field
 sameAngle center a b = (angle center a) == (angle center b)
 
-angle a b = pi/2 + (atan . uncurry (flip (/)) $ (projections a b)) + (if fst a > fst b then pi else 0)
+angle a b = - (uncurry atan2 $ projections a b)
 projections (a, b) (c, d) = (fromIntegral (c-a), fromIntegral (d-b))
 distance (a, b) (c, d) = abs (c-a) + abs (d-b)
 
@@ -25,10 +25,5 @@ display lines targets = foldl insert lines (indexed targets)
   where insert lines (index, (x,y)) = replace y (replace x (intToDigit (index+1)) (lines !! y)) lines
 
 main = do
-    -- contents <- readFile "Day10Sample6.txt"
-    -- let field = asteroidCoordinates contents
-    -- let repr = lines contents
-    -- print $ ((vaporizeOrder (11, 3)) field) !! 199
     contents <- readFile "Day10.txt"
-    -- print $ ((vaporizeOrder (11, 13)) (asteroidCoordinates contents \\ [(11,13)])) !! 199
     print $ ((vaporizeOrder (27, 19)) (asteroidCoordinates contents \\ [(27,19)])) !! 199
