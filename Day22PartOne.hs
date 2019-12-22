abba184c28f7737fa63dc481d07ef2c57266649a
Laurent Bossavit <laurent.git@bossavit.com>
2019-12-22 11:53:46 +0100

    Day 22 part one

---
+++ Day22PartOne.hs
@@ -0,0 +1,43 @@
+module Day22PartOne where
+
+import Common
+import Data.Maybe
+import Data.Tuple
+import Data.List
+import Text.ParserCombinators.ReadP
+
+data Move = Cut Int | Deal Int | NewStack deriving (Eq, Show)
+
+parseCut = do
+    string "cut "
+    result <- number
+    return $ Cut $ fromInteger result
+
+parseNewStack = do
+    string "deal into new stack"
+    return NewStack
+
+parseDeal = do
+    string "deal with increment "
+    result <- number
+    return $ Deal $ fromInteger result
+
+parseShuffle = sepBy1 (choice [parseCut, parseDeal, parseNewStack]) (string "\n")
+
+parse :: String -> [Move]
+parse = fromJust . parseMaybe parseShuffle
+
+result :: [Move] ->  [Int] -> [Int]
+result moves deck = foldl (flip applyMove) deck moves
+
+applyMove :: Move -> [Int] -> [Int]
+applyMove NewStack deck = reverse deck
+applyMove (Cut pos) deck | pos >= 0 = (drop pos deck) ++ (take pos deck)
+applyMove (Cut pos) deck | pos < 0 = (drop (len+pos) deck) ++ (take (len+pos) deck)
+  where len = length deck
+applyMove (Deal pos) deck = map (\p -> deck !! snd p) $ sort $ map (\n -> (n*pos `mod` len, n)) [0..(len-1)]
+  where len = length deck
+
+main = do
+    contents <- readFile "Day22.txt"
+    print $ elemIndex 2019 $ result (parse contents) [0..10006]
---
+++ Day22PartOneSpec.hs
@@ -0,0 +1,18 @@
+module Day22PartOneSpec where
+
+import Day22PartOne hiding (main)
+import Test.Hspec
+
+main = do
+    hspec $
+        describe "Many-Worlds Interpretation" $ do
+            it "Should get the moves right" $ do
+                applyMove (Deal 3) [0..9] `shouldBe` [0, 7, 4, 1, 8, 5, 2, 9, 6, 3]
+                applyMove (Deal 7) [0..9] `shouldBe` [0, 3, 6, 9, 2, 5, 8, 1, 4, 7]
+                applyMove (Cut 3) [0..9] `shouldBe` [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
+                applyMove (Cut (-4)) [0..9] `shouldBe` [6, 7, 8, 9, 0, 1, 2, 3, 4, 5]
+            it "Should get the examples right" $ do
+                result [Deal 7, NewStack, NewStack] [0..9] `shouldBe` [0, 3, 6, 9, 2, 5, 8, 1, 4, 7]
+                result [Cut 6, Deal 7, NewStack] [0..9] `shouldBe` [3, 0, 7, 4, 1, 8, 5, 2, 9, 6]
+                result [Deal 7, Deal 9, Cut (-2)] [0..9] `shouldBe` [6, 3, 0, 7, 4, 1, 8, 5, 2, 9]
+                result [NewStack, Cut (-2), Deal 7, Cut 8, Cut (-4), Deal 7, Cut 3, Deal 9, Deal 3, Cut (-1)] [0..9] `shouldBe` [9, 2, 5, 8, 1, 4, 7, 0, 3, 6]
